64b99e16213279bf9c2e02eaa18f697a1b2793bd
JOSH Haywood <Joshhaywood025@gmail.com>
2023-01-19 23:42:32 +0000

    Update index.tsx formatting.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,27 +10,44 @@ export default function Index() {
   return (
     <>
       <Head>
-        <title>Website name | Home</title>
+        <title>ChatHub | Login</title>
       </Head>
 
       <div className="w-screen h-screen flex justify-center items-center">
         <div className="w-full sm:w-auto flex flex-col justify-center mx-5 sm:mx-0 p-5 sm:p-12 shadow-lg bg-white">
           {/* Heading */}
           <div className="pt-4 mb-4 mx-auto">
-            <Image src={logo} alt="logo" width={75} height={75} />
+            <Image 
+              src={logo} 
+              alt="logo" 
+              width={75} 
+              height={75} 
+            />
           </div>
 
-          <h1 className="mb-3 text-center font-bold text-3xl tracking-wide text-black">Welcome back</h1>
+          <h1 className="mb-3 text-center font-bold text-3xl tracking-wide text-black">
+            Welcome back
+          </h1>
 
-          <div className="text-center tracking-wide text-gray-400">Sign into your account</div>
+          <div className="text-center tracking-wide text-gray-400">
+            Sign into your account
+          </div>
 
           {/* Registration form */}
           <form className="sm:w-[400px] py-8">
             {/* Email */}
-            <Input label="Email Address" type="email" placeholder="email@email.com" />
+            <Input
+              label="Email Address"
+              type="email"
+              placeholder="email@email.com"
+            />
 
             {/* Password */}
-            <Input label="Password" type="password" placeholder="Password123" />
+            <Input 
+              label="Password" 
+              type="password" 
+              placeholder="Password123" 
+            />
 
             {/* Submit button */}
             <div className="mb-6 mt-10 text-center">
@@ -45,10 +62,10 @@ export default function Index() {
 
             {/* Registration link */}
             <hr className="mb-6 border-t" />
-            
+
             <div className="text-center">
               <div className="inline-block tracking-wide text-sm text-gray-400">
-                Don't have an account yet? {" "}
+                Don't have an account yet?{" "}
                 <Link
                   className="inline-block text-sm text-cyan-1000 align-baseline text-blue-500 hover:text-blue-700 hover:underline"
                   href="/register"
