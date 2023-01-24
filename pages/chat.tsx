b12d04e06f5b3f15f1fb7dd210c809bf378f51da
JOSH Haywood <Joshhaywood025@gmail.com>
2023-01-24 22:05:42 +0000

    Add redirect to chat.
    - Added catch to chat page preventing access unless logged in

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,6 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import axios from "axios";
+
 import Chats from "@/components/chat/recipients/Chats";
 
 export default function Chat() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if user is logged in
+    axios.get("/auth/user").then((res) => {
+      // If user is not logged in, redirect to login page
+      if (res.data.loggedIn === false) {
+        router.push("/");
+      };
+    });
+  }, []);
+
   return (
     <div className="w-screen h-screen relative bg-blue-200">
       <div className="absolute left-0 right-0 top-0 bottom-0 m-10 flex flex-row rounded-2xl bg-white">
