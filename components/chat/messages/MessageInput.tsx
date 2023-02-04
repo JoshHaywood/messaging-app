30dc6b7036571f75256ecd560023a4d25412fd5a
Josh Haywood <Joshhaywood025@gmail.com>
2023-02-04 14:38:55 +0000

    Update MessageInput styling.

--- components/chat/messages/MessageInput.tsx
+++ components/chat/messages/MessageInput.tsx
@@ -4,7 +4,7 @@ export default function MessageInput() {
       <input
         type="text"
         placeholder="Type a message..."
-        className="w-full p-2.5 text-sm rounded-lg text-gray-700 bg-gray-100"
+        className="w-full p-2.5 text-sm rounded-lg text-gray-700 bg-gray-100 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-blue-500"
       ></input>
 
       {/* Attribution: https://heroicons.com/ */}
@@ -12,13 +12,13 @@ export default function MessageInput() {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        className="w-8 h-8 text-gray-500 hover:fill-blue-500 hover:text-white hover:cursor-pointer"
+        className="w-8 h-8 text-gray-500 hover:text-blue-500 hover:cursor-pointer"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
         />
       </svg>
